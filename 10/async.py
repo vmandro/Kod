fbf0b8fedd588f6a59bea9c5a1770312bbe84397
Peter Borovansky <peter.borovansky@microstep-mis.com>
2023-11-27 08:02:40 +0100

    desiaty tyzden

---
+++ 10/async.py
@@ -0,0 +1,13 @@
+import asyncio
+
+async def coroutine1():
+    task = asyncio.create_task(coroutine2())
+    await task
+    print(1)
+
+async def coroutine2():
+    print(2)
+    await asyncio.sleep(1)
+   
+asyncio.run(coroutine1())
+print("finito")
